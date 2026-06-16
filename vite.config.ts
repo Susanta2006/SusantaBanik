7839a9085fb54a3ca72bc369409bcb5969665d5d
Susanta Banik <susanta.company4@gmail.com>
2026-06-17 05:27:10 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
-import { nitro } from "nitro/vite"; // <--- Add this import
 
 export default defineConfig({
   plugins: [
@@ -10,7 +9,6 @@ export default defineConfig({
     tanstackStart({
       server: { entry: "server" },
     }),
-    nitro(), // <--- Add this plugin here
     viteReact(),
   ],
   resolve: {
