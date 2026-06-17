a7ea33f0775025c80da05a15189a7facb55898e8
Susanta Banik <susanta.company4@gmail.com>
2026-06-17 05:56:40 +0530

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,8 @@
+// app.config.ts
 import { defineConfig } from "@tanstack/start/config";
-import vercel from "@tanstack/start-adapter-vercel";
 
 export default defineConfig({
   server: {
-    preset: "vercel",
+    preset: "vercel", // <--- This tells Nitro to create Vercel-compatible functions
   },
 });
