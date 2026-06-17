43433958c6acf111ff024a6675caca23d2b515e9
Susanta Banik <susanta.company4@gmail.com>
2026-06-17 06:02:44 +0530

    Delete app.config.ts

--- app.config.ts
+++
@@ -1,8 +0,0 @@
-// app.config.ts
-import { defineConfig } from "@tanstack/start/config";
-
-export default defineConfig({
-  server: {
-    preset: "vercel", // <--- This tells Nitro to create Vercel-compatible functions
-  },
-});
